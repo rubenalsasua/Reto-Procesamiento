41d32c845469a134ac9b11bd2a0edd0ac3ae937b
Rubén Javier Alsasua Pérez <r.alsasua@opendeusto.es>
2025-03-20 15:44:55 +0100

    Error solucionado

--- Validacion/models.py
+++ Validacion/models.py
@@ -1,6 +1,7 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, field_validator
 from datetime import datetime
 from typing import Union
+
 
 class DatosGenerador(BaseModel):
     id_generador: int = Field(..., example=1)
@@ -8,11 +9,11 @@ class DatosGenerador(BaseModel):
     velocidad_viento: Union[float, str] = Field(..., example=12.5)
     temperatura: Union[float, str] = Field(..., example=25.2)
     timestamp: str = Field(..., example="2024-03-17T12:00:00Z")
-    
-    @validator("potencia_kw")
+
+    @field_validator("potencia_kw")
+    @classmethod
     def validar_potencia(cls, v):
         if isinstance(v, (int, float)):  # Verifica si es un número
             if v >= 4000:
                 raise ValueError("La potencia debe ser menor que 4000 kW")
         return v
-
