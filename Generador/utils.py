30f94eb17b74a198600bf99cfe95dd95c83cb8cf
Eneko Saez <eneko.saez.merino@opendeusto.es>
2025-03-23 21:20:45 +0100

    cambios y presentacion

--- Generador/utils.py
+++ Generador/utils.py
@@ -7,15 +7,15 @@ def generar_datos_sinteticos(id_generador, probabilidad_error=0.1):
     """
     datos = {
         "id_generador": id_generador,
-        "potencia_kw": round(np.random.uniform(0, 5000), 2),  # Potencia entre 0 y 5000 kW
-        "velocidad_viento": round(np.random.uniform(2, 25), 2),  # Velocidad entre 2 y 25 m/s
-        "temperatura": round(np.random.uniform(-10, 50), 2),  # Temperatura entre -10 y 50 °C
-        "timestamp": np.datetime64('now').astype(str)  # Timestamp actual
+        "potencia_kw": round(np.random.uniform(0, 5000), 2),  
+        "velocidad_viento": round(np.random.uniform(2, 25), 2),  
+        "temperatura": round(np.random.uniform(-10, 50), 2), 
+        "timestamp": np.datetime64('now').astype(str)  
     }
 
     # Introducir error aleatorio con probabilidad N
     if random.random() < probabilidad_error:
         clave_erronea = random.choice(["potencia_kw", "velocidad_viento", "temperatura"])
-        datos[clave_erronea] = "ERROR"  # Insertar un valor inválido
+        datos[clave_erronea] = "ERROR"  
 
     return datos
