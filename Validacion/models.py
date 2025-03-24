2f848a3e41d66b876e707311f1eb207ed0cc16a3
Eneko Saez <eneko.saez.merino@opendeusto.es>
2025-03-24 09:38:48 +0100

    ultimos cambios

--- Validacion/models.py
+++ Validacion/models.py
@@ -10,10 +10,4 @@ class DatosGenerador(BaseModel):
     temperatura: Union[float, str] = Field(..., example=25.2)
     timestamp: str = Field(..., example="2024-03-17T12:00:00Z")
 
-    @field_validator("potencia_kw")
-    @classmethod
-    def validar_potencia(cls, v):
-        if isinstance(v, (int, float)):  # Verifica si es un número
-            if v >= 4000:
-                raise ValueError("La potencia debe ser menor que 4000 kW")
-        return v
+  
